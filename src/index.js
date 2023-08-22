301b9e77f3de0d320e1adf2d59b52497ba7ad2fb
Naim Miah <mmiah@fordham.edu>
2023-08-22 15:02:03 -0400

    added grids

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./ttt.css";
-
-console.log("Hello World!");
+import Game from "./ttt_node/game";
+import View from "./ttt-view";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+
+  const game = new Game();
+  const el = document.querySelector(".ttt");
+  const view = new View(game, el);
+  window.view = view;
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,18 +1,30 @@
 class View {
   constructor(game, el) {
-  }
-  
-  setupBoard() {
-  }
-  
-  handleClick(e) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
   }
 
-  makeMove(square) {
+  // setup html view of board, 3X3 tictactoe board
+  setupBoard() {
+    let ul = document.createElement("ul");
+    ul.classList.add("grid");
+
+    for (let i = 0; i < 9; i++) {
+      let li = document.createElement("li");
+      li.classList.add("square");
+      li.setAttribute("data-pos", [Math.floor(i / 3), i % 3]);
+      ul.appendChild(li);
+    }
+
+    this.el.appendChild(ul);
   }
-  
-  handleGameOver() {
-  }
+
+  handleClick(e) {}
+
+  makeMove(square) {}
+
+  handleGameOver() {}
 }
 
 export default View;
